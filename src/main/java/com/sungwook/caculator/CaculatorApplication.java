9422afbc108024ddfa5791a754c0d1316ecf6e26
choisungwook <kgg1959@naver.com>
2020-08-09 17:55:36 +0900

    프로젝트 생성

---
+++ src/main/java/com/sungwook/caculator/CaculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sungwook.caculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungwook/caculator/CaculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungwook.caculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
